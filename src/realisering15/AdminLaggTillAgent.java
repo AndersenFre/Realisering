8af57c2553faa0aa8cd3b6498df79b2ef53bcd2d
oliverfreij <oliverfreij@gmail.com>
2023-01-03 12:08:33 +0100

    Update AdminLaggTillAgent.java

--- src/realisering15/AdminLaggTillAgent.java
+++ src/realisering15/AdminLaggTillAgent.java
@@ -15,14 +15,14 @@ public class AdminLaggTillAgent extends javax.swing.JFrame {
 
     private static InfDB databas;
     private String id;
-    
+
     /**
      * Creates new form AdminLaggTillAgent
      */
     public AdminLaggTillAgent(InfDB databas, String id) {
         initComponents();
         this.databas = databas;
-        this.id=id;
+        this.id = id;
     }
 
     /**
@@ -169,32 +169,27 @@ public class AdminLaggTillAgent extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnLaggTillAgentTillbakaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLaggTillAgentTillbakaActionPerformed
-        // TODO add your handling code here:
         AdminLaggTillAgent.this.dispose();
     }//GEN-LAST:event_btnLaggTillAgentTillbakaActionPerformed
 
     private void btnLaggTillActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLaggTillActionPerformed
-        // TODO add your handling code here:
-        
-        if(Validering.isHeltal(txtNyttID) && Validering.textFaltHarVarde(txtNyttNamn) && Validering.textFaltHarVarde(txtNyTelNr) && Validering.textFaltHarVarde(txtNyttLosen) && Validering.textFaltHarVarde(txtNyAdminStatus) && Validering.textFaltHarVarde(txtNyttAnstallningsdatum) && Validering.isHeltal(txtNyttOmrade)){
-        try{
-           
-        String nyttID = txtNyttID.getText();
-        String nyttNamn = txtNyttNamn.getText();
-        String TelNr = txtNyTelNr.getText();
-        String nyttLosen = txtNyttLosen.getText();
-        String nyAdminStatus = txtNyAdminStatus.getText();
-        String nyttAnstallningsdatum = txtNyttAnstallningsdatum.getText();
-        String nyttOmrade = txtNyttOmrade.getText();
-        String allaVarden = "insert into agent values(" +nyttID+ ",'" +nyttNamn+ "', '" +TelNr+ "', '" +nyttAnstallningsdatum+ "', '" +nyAdminStatus+ "', '" +nyttLosen+ "', " +nyttOmrade+ ")";
-        
-        databas.insert(allaVarden);
-        JOptionPane.showMessageDialog(null, ""+nyttNamn+" har registrerats.");
-        }
-        catch(InfException ex){
-        JOptionPane.showMessageDialog(null, "Registreringen misslyckades, försök igen");
-        System.out.println("Internt felmeddelande" + ex.getMessage());
-        }
+        if (Validering.isHeltal(txtNyttID) && Validering.textFaltHarVarde(txtNyttNamn) && Validering.textFaltHarVarde(txtNyTelNr) && Validering.textFaltHarVarde(txtNyttLosen) && Validering.textFaltHarVarde(txtNyAdminStatus) && Validering.textFaltHarVarde(txtNyttAnstallningsdatum) && Validering.isHeltal(txtNyttOmrade)) {
+            try {
+                String nyttID = txtNyttID.getText();
+                String nyttNamn = txtNyttNamn.getText();
+                String TelNr = txtNyTelNr.getText();
+                String nyttLosen = txtNyttLosen.getText();
+                String nyAdminStatus = txtNyAdminStatus.getText();
+                String nyttAnstallningsdatum = txtNyttAnstallningsdatum.getText();
+                String nyttOmrade = txtNyttOmrade.getText();
+                String allaVarden = "insert into agent values(" + nyttID + ",'" + nyttNamn + "', '" + TelNr + "', '" + nyttAnstallningsdatum + "', '" + nyAdminStatus + "', '" + nyttLosen + "', " + nyttOmrade + ")";
+
+                databas.insert(allaVarden);
+                JOptionPane.showMessageDialog(null, "" + nyttNamn + " har registrerats.");
+            } catch (InfException ex) {
+                JOptionPane.showMessageDialog(null, "Registreringen misslyckades, försök igen");
+                System.out.println("Internt felmeddelande" + ex.getMessage());
+            }
         }
     }//GEN-LAST:event_btnLaggTillActionPerformed
 
