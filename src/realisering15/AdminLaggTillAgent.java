4ff3129a62a488038db6debffa8251c2a086f6d6
Felix Lundqvist <felix.b.lundqvist@hotmail.se>
2023-01-03 12:05:16 +0100

    klar med adminlaggtillagent

--- src/realisering15/AdminLaggTillAgent.java
+++ src/realisering15/AdminLaggTillAgent.java
@@ -4,8 +4,7 @@
  */
 package realisering15;
 
-import java.util.ArrayList;
-import javax.swing.JComboBox;
+
 import oru.inf.InfDB;
 import oru.inf.InfException;
 import javax.swing.JOptionPane;
@@ -36,17 +35,55 @@ public class AdminLaggTillAgent extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        lblLaggTillA = new javax.swing.JLabel();
         btnLaggTillAgentTillbaka = new javax.swing.JButton();
+        lblID = new javax.swing.JLabel();
+        lblNamn = new javax.swing.JLabel();
+        lblTelNr = new javax.swing.JLabel();
+        lblAnstallningsdatum = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        lblAdminStatus = new javax.swing.JLabel();
+        lblLosenord = new javax.swing.JLabel();
+        lblOmrade = new javax.swing.JLabel();
+        btnLaggTill = new javax.swing.JButton();
+        txtNyttID = new javax.swing.JTextField();
+        txtNyttNamn = new javax.swing.JTextField();
+        txtNyTelNr = new javax.swing.JTextField();
+        txtNyttAnstallningsdatum = new javax.swing.JTextField();
+        txtNyAdminStatus = new javax.swing.JTextField();
+        txtNyttLosen = new javax.swing.JTextField();
+        txtNyttOmrade = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Lagg till Agent");
+        lblLaggTillA.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
+        lblLaggTillA.setText("Lägg till Agent");
 
         btnLaggTillAgentTillbaka.setText("Tillbaka");
         btnLaggTillAgentTillbaka.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnLaggTillAgentTillbakaActionPerformed(evt);
+            }
+        });
+
+        lblID.setText("ID:");
+
+        lblNamn.setText("Namn:");
+
+        lblTelNr.setText("Telefon:");
+
+        lblAnstallningsdatum.setText("Anställningsdatum:");
+
+        lblAdminStatus.setText("Administratör:");
+
+        lblLosenord.setText("Lösenord:");
+
+        lblOmrade.setText("Område:");
+
+        btnLaggTill.setText("Lägg Till");
+        btnLaggTill.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLaggTillActionPerformed(evt);
             }
         });
 
@@ -57,21 +94,76 @@ public class AdminLaggTillAgent extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(162, 162, 162)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(btnLaggTillAgentTillbaka)))
-                .addContainerGap(161, Short.MAX_VALUE))
+                        .addComponent(btnLaggTillAgentTillbaka)
+                        .addGap(218, 218, 218))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(146, 146, 146)
+                            .addComponent(lblLaggTillA))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(lblID)
+                                .addComponent(lblTelNr)
+                                .addComponent(lblAnstallningsdatum, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(lblNamn)
+                                .addComponent(txtNyttAnstallningsdatum)
+                                .addComponent(txtNyTelNr)
+                                .addComponent(txtNyttNamn)
+                                .addComponent(txtNyttID))
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
+                            .addComponent(jLabel3)
+                            .addGap(87, 87, 87))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnLaggTill)
+                    .addComponent(lblAdminStatus)
+                    .addComponent(lblLosenord)
+                    .addComponent(lblOmrade)
+                    .addComponent(txtNyttOmrade, javax.swing.GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
+                    .addComponent(txtNyttLosen)
+                    .addComponent(txtNyAdminStatus))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(102, 102, 102)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
-                .addComponent(btnLaggTillAgentTillbaka)
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addComponent(lblLaggTillA)
+                .addGap(4, 4, 4)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(lblAdminStatus)
+                    .addComponent(lblID))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNyttID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNyAdminStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblNamn)
+                    .addComponent(lblLosenord))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNyttNamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNyttLosen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTelNr)
+                    .addComponent(lblOmrade))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNyTelNr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNyttOmrade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblAnstallningsdatum)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtNyttAnstallningsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnLaggTillAgentTillbaka)
+                    .addComponent(btnLaggTill))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -81,8 +173,50 @@ public class AdminLaggTillAgent extends javax.swing.JFrame {
         // TODO add your handling code here:
         AdminLaggTillAgent.this.dispose();
     }//GEN-LAST:event_btnLaggTillAgentTillbakaActionPerformed
+
+    private void btnLaggTillActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLaggTillActionPerformed
+        // TODO add your handling code here:
+        
+        if(Validering.isHeltal(txtNyttID) && Validering.textFaltHarVarde(txtNyttNamn) && Validering.textFaltHarVarde(txtNyTelNr) && Validering.textFaltHarVarde(txtNyttLosen) && Validering.textFaltHarVarde(txtNyAdminStatus) && Validering.textFaltHarVarde(txtNyttAnstallningsdatum) && Validering.isHeltal(txtNyttOmrade)){
+        try{
+           
+        String nyttID = txtNyttID.getText();
+        String nyttNamn = txtNyttNamn.getText();
+        String TelNr = txtNyTelNr.getText();
+        String nyttLosen = txtNyttLosen.getText();
+        String nyAdminStatus = txtNyAdminStatus.getText();
+        String nyttAnstallningsdatum = txtNyttAnstallningsdatum.getText();
+        String nyttOmrade = txtNyttOmrade.getText();
+        String allaVarden = "insert into agent values(" +nyttID+ ",'" +nyttNamn+ "', '" +TelNr+ "', '" +nyttAnstallningsdatum+ "', '" +nyAdminStatus+ "', '" +nyttLosen+ "', " +nyttOmrade+ ")";
+        
+        databas.insert(allaVarden);
+        JOptionPane.showMessageDialog(null, ""+nyttNamn+" har registrerats.");
+        }
+        catch(InfException ex){
+        JOptionPane.showMessageDialog(null, "Registreringen misslyckades, försök igen");
+        System.out.println("Internt felmeddelande" + ex.getMessage());
+        }
+        }
+    }//GEN-LAST:event_btnLaggTillActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLaggTill;
     private javax.swing.JButton btnLaggTillAgentTillbaka;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel lblAdminStatus;
+    private javax.swing.JLabel lblAnstallningsdatum;
+    private javax.swing.JLabel lblID;
+    private javax.swing.JLabel lblLaggTillA;
+    private javax.swing.JLabel lblLosenord;
+    private javax.swing.JLabel lblNamn;
+    private javax.swing.JLabel lblOmrade;
+    private javax.swing.JLabel lblTelNr;
+    private javax.swing.JTextField txtNyAdminStatus;
+    private javax.swing.JTextField txtNyTelNr;
+    private javax.swing.JTextField txtNyttAnstallningsdatum;
+    private javax.swing.JTextField txtNyttID;
+    private javax.swing.JTextField txtNyttLosen;
+    private javax.swing.JTextField txtNyttNamn;
+    private javax.swing.JTextField txtNyttOmrade;
     // End of variables declaration//GEN-END:variables
 }
